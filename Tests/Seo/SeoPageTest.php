7245ee2224a44d0ca6d6c41a82b96801b6316dee
Jaz <jayesbe@users.noreply.github.com>
2016-08-23 20:04:59 -0400

    add test for normalize fix
    https://github.com/sonata-project/SonataSeoBundle/pull/121/commits/eb6425d83c0418f43e17122b809fec9db7bb7d64

--- Tests/Seo/SeoPageTest.php
+++ Tests/Seo/SeoPageTest.php
@@ -40,9 +40,22 @@ class SeoPageTest extends \PHPUnit_Framework_TestCase
             'property' => array('foo' => array('bar', array()), 'foo2' => array('bar2', array())),
         );
 
+        $expected = array(
+            'property' => array('foo' => array('bar', array()), 'foo2' => array('bar2', array())),
+        );
         $this->assertEquals($expected, $page->getMetas());
     }
-
+    
+    public function testAcceptedMetaContent()
+    {
+        $page = new SeoPage();
+        $page->setMetas(array('property' => array('int' => 123, 'string' => 'i am a string', 'float' => 55.22)));
+        $expected = array(
+            'property' => array('int' => array(123, array()), 'string' => array('i am a string', array()), 'float' => array(55.22, array())),
+        );
+        $this->assertEquals($expected, $page->getMetas());
+    }
+    
     public function testRemoveMeta()
     {
         $page = new SeoPage();
